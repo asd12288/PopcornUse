4c5a1e3ea36cd37dcf838b082e0f8d380da0c19c
asd12288 <ilanchelly@gmail.com>
2024-12-30 11:25:38 +0200

    updated

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { use, useEffect, useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import StarRating from "./StarsRating";
 import { useMovies } from "./useMovies";
 import { useLocalStorageState } from "./useLocaleStorageState";
